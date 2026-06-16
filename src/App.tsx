294f93d2070b25c8369497c0dd8028dd03981132
leeluse <lgh0385hh@gmail.com>
2026-06-16 17:20:21 +0900

    01: Tic-Tac-Toe

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import TicTacToc from './01-tictactoe/TicTacToc'
+import './App.css'
+
+function App() {
+
+  return (
+    <main className='container'>
+      <div className='content'>
+        <TicTacToc />
+      </div>
+    </main>
+  )
+}
+
+export default App
